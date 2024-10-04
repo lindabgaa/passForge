4c66b1a4591ee9460f46d900e2b499969b441a80
Linda BOUGAA <lindabgaa@Host-002.lan>
2024-10-04 20:42:25 +0200

    a lot of code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// tools
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+// assets
+import copyIcon from "./assets/copy-icon.svg";
+
+// styles
+import "./App.css";
+
+export default function App() {
+  const [password, setPassword] = useState("P4$5W0rD!");
+  const [length, setLength] = useState(0);
+  const [options, setOptions] = useState({
+    uppercase: false,
+    lowercase: false,
+    numbers: false,
+    symbols: false,
+  });
+
+  const handleRangeChange = (e) => {
+    setLength(e.currentTarget.value);
+  };
+
+  const handleCopyIconClick = () => {
+    navigator.clipboard.writeText(password);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="main-container">
+      <div className="display-wrapper">
+        <p>{password}</p>
+        <div className="copy-wrapper">
+          <p></p>
+          <img
+            src={copyIcon}
+            className="copy-icon"
+            alt="copy icon"
+            onClick={handleCopyIconClick}
+          />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <form action="" className="form-wrapper">
+        <div className="length-wrapper">
+          <p>Character</p>
+          <input
+            className="length-slider"
+            type="range"
+            min="0"
+            max="100"
+            value={length}
+            onChange={handleRangeChange}
+          ></input>
+          <div className="length-value">{length}</div>
+        </div>
 
-export default App
+        <ul className="options-list">
+          <li className="option-item">
+            <label htmlFor="uppercase">Uppercase Letters</label>
+            <input type="checkbox" id="uppercase"></input>
+          </li>
+          <li className="option-item">
+            <label htmlFor="lowercase">Lowercase Letters</label>
+            <input type="checkbox" id="lowercase"></input>
+          </li>
+          <li className="option-item">
+            <label htmlFor="number">Numbers</label>
+            <input type="checkbox" id="number"></input>
+          </li>
+          <li className="option-item">
+            <label htmlFor="symbols">Symbols (@&$!#?)</label>
+            <input type="checkbox" id="symbols"></input>
+          </li>
+        </ul>
+      </form>
+    </main>
+  );
+}
